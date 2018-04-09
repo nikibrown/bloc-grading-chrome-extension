31f589afe4130e72cd6f557bcaaf21a2d58cdc94
Niki Brown <hi@nikibrown.com>
2018-04-09 08:09:43 -0400

    First pass at grading chrome ext

---
+++ README.md
@@ -0,0 +1,17 @@
+## Bloc Grading Chrome Extension
+
+Chrome extension for Bloc graders to auto fill with default grading message and students name.
+
+- Only works when `#submission` is at the end of URL (or textarea is visible on the page): [https://www.bloc.io/users/nick-noel/checkpoints/2017#submission](https://www.bloc.io/users/nick-noel/checkpoints/2017#submission) (ie you are clicking on a link from the grading spreadsheet)
+
+
+### Installation instructions
+
+- Download or clone this repo
+- In Chrome go to [chrome://extensions/](chrome://extensions/)
+- Turn on developer mode
+- ![developer mode](http://nikib.ro/wn/screenshots/Extensions_2018-04-09_08-05-18.jpg)
+- Click on `load unpacked` and browse to the extension files that you cloned or downloaded
+- Thats it!
+
+- Feel free to comment out the alert on line 2 of grading-msg.js and customize your own grading message
---
+++ grading-msg.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    alert("Grading extension is running");
+    // grab the HTML element that has the students name in it
+    var studentNameContainer = document.getElementsByClassName('mentor-review-header');
+
+    // get the text
+    var studentNameSentence = studentNameContainer[0].innerText;
+    // split the text
+    var splitString = studentNameSentence.split(" ");
+
+    // grab the name!
+    var studentName = splitString[2];
+
+    // grading message goes here
+    var gradingMessage = "Hi " + studentName + "! \n\nNiki from the grading team here.\n\nIf anything here that I’ve mentioned is unclear, please don’t hesitate to [reach out for help via Slack.](https://www.bloc.io/resources/getting-unstuck) \n\nThanks, Niki";
+
+    // get textzarea
+    var submissionTextarea = document.getElementById('comment-box');
+
+    // set value of text area with student name
+    submissionTextarea.value = gradingMessage;
+};
