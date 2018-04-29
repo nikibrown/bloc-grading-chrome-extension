cdc555197aa5b2d9133149ba1a4500ff3b018731
Niki Brown <hi@nikibrown.com>
2018-04-29 11:37:45 -0400

    add rawgit functionamlity, wrapped in if statement

--- grading-msg.js
+++ grading-msg.js
@@ -1,37 +1,42 @@
 window.onload = function() {
-    console.log("Grading extension is running");
-    // grab the HTML element that has the students name in it
-    var studentNameContainer = document.getElementsByClassName('mentor-review-header');
+    if(window.location.href.indexOf("bloc") > -1) {
+        console.log("Grading extension is running");
+        // grab the HTML element that has the students name in it
+        var studentNameContainer = document.getElementsByClassName('mentor-review-header');
 
-    // get the text
-    var studentNameSentence = studentNameContainer[0].innerText;
-    // split the text
-    var splitString = studentNameSentence.split(" ");
+        // get the text
+        var studentNameSentence = studentNameContainer[0].innerText;
+        // split the text
+        var splitString = studentNameSentence.split(" ");
 
-    // grab the name!
-    var studentName = splitString[2];
+        // grab the name!
+        var studentName = splitString[2];
 
-    // grading message goes here
-    var gradingMessage = "Hi " + studentName + "! \n\nNiki from the grading team here.\n\nIf anything here that I’ve mentioned is unclear, please don’t hesitate to [reach out for help via Slack.](https://www.bloc.io/resources/getting-unstuck) \n\nThanks, Niki";
+        // grading message goes here
+        var gradingMessage = "Hi " + studentName + "! \n\nNiki from the grading team here.\n\nIf anything here that I’ve mentioned is unclear, please don’t hesitate to [reach out for help via Slack.](https://www.bloc.io/resources/getting-unstuck) \n\nThanks, Niki";
 
-    // get textzarea
-    var submissionTextarea = document.getElementById('comment-box');
+        // get textzarea
+        var submissionTextarea = document.getElementById('comment-box');
 
-    // set value of text area with student name
-    submissionTextarea.value = gradingMessage;
-
-    // rawgit.com url magic
-    let urlProdInput = document.querySelector("#url-prod");
-
-    let urlPasted = document.querySelector("#url");
-
-    var changeURL = function () {
-        document.location.href = urlProdInput.value;
+        // set value of text area with student name
+        submissionTextarea.value = gradingMessage;
     }
 
-    urlPasted.addEventListener("input", function() {
-        // needed to use setTimeout because the prodURL does not populate right away
-        setTimeout(changeURL, 100);
-    });
+    // rawgit.com url magic
+    if(window.location.href.indexOf("rawgit") > -1) {
+        console.log("rawgit js");
+        let urlProdInput = document.querySelector("#url-prod");
+
+        let urlPasted = document.querySelector("#url");
+
+        var changeURL = function () {
+            document.location.href = urlProdInput.value;
+        }
+
+        urlPasted.addEventListener("input", function() {
+            // needed to use setTimeout because the prodURL does not populate right away
+            setTimeout(changeURL, 200);
+        });
+    }
 
 };
