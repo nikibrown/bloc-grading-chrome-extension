8c7f7a4739fbd87ad9210750ca2007526b3adf4f
Obed Parlapiano <obed.marquezp@gmail.com>
2019-02-03 08:21:05 +0100

    Add support for thinkful grading.

--- grading-msg.js
+++ grading-msg.js
@@ -1,45 +1,68 @@
-window.onload = function() {
-    if(window.location.href.includes("bloc")) {
-        console.log("Grading extension is running");
-        // grab the HTML element that has the students name in it
-        const studentNameContainer = document.getElementsByClassName('mentor-review-header');
+  window.onload = function () {
+    console.log("Grading extension is running");
 
-        // get the text
-        const studentNameSentence = studentNameContainer[0].innerText;
-        // split the text
-        const splitString = studentNameSentence.split(" ");
+    const isBloc = window.location.href.includes('bloc');
+    const isThinkful = window.location.href.includes('thinkful');
+    // Your name here!
+    const graderName = 'Niki';
 
-        // grab the name!
-        const studentName = splitString[2];
+    if (isBloc) {
+      // grab the HTML element that has the students name in it
+      const studentNameContainer = document.getElementsByClassName('mentor-review-header');
 
-        // Your name here!
-        const graderName = "Niki";
+      // get the text
+      const studentNameSentence = studentNameContainer[0].innerText;
+      // split the text
+      const splitString = studentNameSentence.split(' ');
 
+      // grab the name!
+      const studentName = splitString[2];
+
+      // grading message goes here
+      const gradingMessage = `Hi ${studentName}! ${graderName} from the grading team here!\n\n***\nIf anything here that I’ve mentioned is unclear, please don’t hesitate to [reach out for help via Slack.](https://www.bloc.io/resources/getting-unstuck) \n\nThanks, **${graderName}**\n\n_Granding Team Member_`;
+
+      // get textzarea
+      const submissionTextarea = document.getElementById('comment-box');
+
+      // set value of text area with student name
+      submissionTextarea.value = gradingMessage;
+    }
+
+    if (isThinkful) {
+      // get textzarea
+      const submissionTextarea = document.getElementById('content');
+
+      if(submissionTextarea) {
+        // Ge the student's name (which doesn't have a specific class :( ).
+        const fullStudentName =
+          document.getElementsByClassName('submission-grader-card-top-bar')[0]
+            .getElementsByTagName('a')[0]
+            .innerHTML;
+
+        // Take only the first name
+        const studentName = fullStudentName.split(" ")[0];
         // grading message goes here
-        const gradingMessage = `Hi ${studentName}! ${graderName} from the grading team here!\n\n***\nIf anything here that I’ve mentioned is unclear, please don’t hesitate to [reach out for help via Slack.](https://www.bloc.io/resources/getting-unstuck) \n\nThanks, ${graderName}`
-
-        // get textzarea
-        const submissionTextarea = document.getElementById('comment-box');
-
+        const gradingMessage = `Hi ${studentName}! ${graderName} from the grading team here!\n\n***\nIf anything here that I’ve mentioned is unclear, please don’t hesitate to reach out for help via Slack. \n\nThanks, ${graderName}\n\nGranding Team Member`;
         // set value of text area with student name
         submissionTextarea.value = gradingMessage;
+      }
     }
 
     // rawgit.com url magic
-    if(window.location.href.indexOf("rawgit") > -1) {
-        console.log("rawgit js");
-        const urlProdInput = document.querySelector("#url-prod");
+    if (window.location.href.indexOf('rawgit') > -1) {
+      console.log('rawgit js');
+      const urlProdInput = document.querySelector('#url-prod');
 
-        const urlPasted = document.querySelector("#url");
+      const urlPasted = document.querySelector('#url');
 
-        const changeURL = function () {
-            document.location.href = urlProdInput.value;
-        }
+      const changeURL = function () {
+        document.location.href = urlProdInput.value;
+      };
 
-        urlPasted.addEventListener("input", function() {
-            // needed to use setTimeout because the prodURL does not populate right away
-            setTimeout(changeURL, 200);
-        });
+      urlPasted.addEventListener('input', function () {
+        // needed to use setTimeout because the prodURL does not populate right away
+        setTimeout(changeURL, 200);
+      });
     }
 
-};
+  };
