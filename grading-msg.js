cb728b98d8de7fc8d297d73432adad4a31ddcd19
Obed <obed.imp@gmail.com>
2019-03-23 00:32:49 -0400

    Use only 2 options: "dev" and "design". Add message accordingly

--- grading-msg.js
+++ grading-msg.js
@@ -1,16 +1,17 @@
 
 // This block needs to be before the window,onload as the chrome storage it's async.
 let graderName = '';
-let blocGrader = false;
-let thinkfulDev = false;
-let thinkfulDesign = false;
+let devGrader = false;
+let designGrader = false;
 
  chrome.storage.sync.get('graderName', (data) => graderName = data.graderName);
- chrome.storage.sync.get('graderType', (data) => blocGrader = data.graderType === 'bloc-grader');
- chrome.storage.sync.get('graderType', (data) => thinkfulDev = data.graderType === 'thinkful-dev');
- chrome.storage.sync.get('graderType', (data) => thinkfulDesign = data.graderType === 'thinkful-design');
+ chrome.storage.sync.get('graderType', (data) => devGrader = data.graderType === 'dev-grader');
+ chrome.storage.sync.get('graderType', (data) => designGrader = data.graderType === 'design-grader');
 
-  window.onload =  function () {
+ const getGraderSignature = () =>   `\n\nThanks, ${graderName && `**${graderName}**`}\n\n_Grading Team Member_`;
+const getIntroText = (studentName) => `Hi ${studentName}! ${graderName && `${graderName} from the grading team here!`} \n\n***\n`;
+
+window.onload =  function () {
     const isBloc = window.location.href.includes('bloc');
     const isThinkful = window.location.href.includes('thinkful');
 
@@ -25,30 +26,15 @@ let thinkfulDesign = false;
 
       // grab the name!
       const studentName = splitString[2];
-
-      // grading message goes here
-      console.log(graderName);
-      const graderSignature = `\n\nThanks, **${graderName}**\n\n_Grading Team Member_`
-      const introSignature = `Hi ${studentName}! ${graderName} from the grading team here! \n\n***\n`;
-
-      const blocGraderMessage = `${introSignature}If anything here that I’ve mentioned is unclear, please don’t hesitate to [reach out for help via Slack.](https://www.bloc.io/resources/getting-unstuck) ${graderSignature}`;
-      const thinkfulDevMessage = `${introSignature}If anything here that I’ve mentioned is unclear, please don’t hesitate to join an Q&A session for technical assistance via [Slack](https://www.thinkful.com/open-sessions/qa-sessions/). If it’s a question about the feedback, feel free to resubmit with a question and the grading team will get back to you as quickly as possible. ${graderSignature}`;
-      const thinkfulDesignMessage = `${introSignature}If anything here that I’ve mentioned is unclear, please don’t hesitate to reach out for technical assistance via Slack in the [#product-design channel](https://thinkful.slack.com/messages/product-design/). If it’s a question about the feedback, feel free to resubmit with a question. ${graderSignature}`;
-      let gradingMessage =  '';
-
-      if (blocGrader) {
-        gradingMessage = blocGraderMessage;
-      } else if (thinkfulDev) {
-        gradingMessage = thinkfulDevMessage;
-      }else if (thinkfulDesign) {
-        gradingMessage = thinkfulDesignMessage;
-      }
+      const blocDevGraderMessage = `${getIntroText(studentName)}If anything here that I’ve mentioned is unclear, please don’t hesitate to [reach out for help via Slack.](https://www.bloc.io/resources/getting-unstuck) ${getGraderSignature()}`;
 
       // get textzarea
       const submissionTextarea = document.getElementById('comment-box');
 
-      // set value of text area with student name
-      submissionTextarea.value = gradingMessage;
+      if(submissionTextarea) {
+        // set value of text area with student name
+        submissionTextarea.value = blocDevGraderMessage;
+      }
     }
 
     if (isThinkful) {
@@ -66,11 +52,17 @@ let thinkfulDesign = false;
 
         // Take only the first name
         const studentName = fullStudentName.split(' ')[0];
-        const thinkfulSlackLink = "https://thinkful.slack.com/messages/general-discussion/";
-        // grading message goes here
-        const gradingMessage = `Hi ${studentName}! ${graderName} from the grading team here!\n\n${contentInTextArea}\n\n***\nIf anything here that I’ve mentioned is unclear, please don’t hesitate to [reach out for help via Slack.](${thinkfulSlackLink}) \n\nThanks, **${graderName}**\n\nGrading Team Member`;
-        // set value of text area with student name
-        submissionTextarea.value = gradingMessage;
+        const thinkfulDevMessage = `${getIntroText(studentName)}${contentInTextArea}If anything here that I’ve mentioned is unclear, please don’t hesitate to join an Q&A session for technical assistance via [Slack](https://www.thinkful.com/open-sessions/qa-sessions/). If it’s a question about the feedback, feel free to resubmit with a question and the grading team will get back to you as quickly as possible. ${getGraderSignature()}`;
+        const thinkfulDesignMessage = `${getIntroText(studentName)}${contentInTextArea}If anything here that I’ve mentioned is unclear, please don’t hesitate to reach out for technical assistance via Slack in the [#product-design channel](https://thinkful.slack.com/messages/product-design/). If it’s a question about the feedback, feel free to resubmit with a question. ${getGraderSignature()}`;
+
+        if (devGrader) {
+          submissionTextarea.value = thinkfulDevMessage;
+        } else if (designGrader) {
+          submissionTextarea.value = thinkfulDesignMessage;
+        } else {
+          submissionTextarea.value = 'Please select your grader type in the chrome extension' +
+            ' settings';
+        }
       }
     }
 
