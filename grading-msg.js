1d17299b489b3fccd23555d3a59d1f4c0dd0246a
Obed <obed.parlapiano@leovegas.com>
2019-04-12 21:32:26 +0200

    Add correct message based on the grading platform

--- grading-msg.js
+++ grading-msg.js
@@ -1,5 +1,5 @@
 const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-const getGraderSignature = (graderName) => `\n\nThanks, ${graderName && `**${graderName}**`}\n\n_Grading Team Member_`;
+const getGraderSignature = (graderName) => `\nThanks, ${graderName && `__${graderName}__`}`;
 const separator = `\n\n***\n`;
 
 window.onload = function () {
@@ -10,6 +10,14 @@ window.onload = function () {
     const graderName = data.graderName || '';
     const devGrader = data.graderType === 'dev-grader';
     const designGrader = data.graderType === 'design-grader';
+    const isBlocGrading = data.gradingPlatform === 'bloc-platform';
+    const isThinkfulGrading= data.gradingPlatform === 'thinkful-platform';
+    const isBlocWebsite = window.location.href.includes('bloc');
+    const isThinkfulWebsite = window.location.href.includes('thinkful');
+
+    const blocDevGraderMessage = studentName =>
+      `${getIntroText(graderName, studentName)}${separator}If anything here that I’ve mentioned is unclear, please don’t hesitate to [reach out for help via Slack.](http://bit.ly/bloc-grading-unstuck) \n\nWant to learn more? Check out our [group sessions & QA resources page](http://bit.ly/gs-g-home) with hours of recorded video and live sessions.\n ${getGraderSignature(graderName)}`;
+
     // Use the customized intro message if there's one, otherwise the default.
     const getIntroText = (graderName, studentName) => {
       const customIntro = data.introMessage && data.introMessage
@@ -19,10 +27,7 @@ window.onload = function () {
       return customIntro ? `${customIntro}\n\n` : `Hi ${studentName}! ${graderName && `${graderName} from the grading team here!`}\n\n`;
     };
 
-    const isBloc = window.location.href.includes('bloc');
-    const isThinkful = window.location.href.includes('thinkful');
-
-    if (isBloc) {
+    if (isBlocWebsite) {
       // grab the HTML element that has the students name in it
       const studentNameContainer = document.getElementsByClassName('mentor-review-header');
 
@@ -33,18 +38,15 @@ window.onload = function () {
 
       // grab the name!
       const studentName = capitalizeFirstLetter(splitString[2]);
-      const blocDevGraderMessage = `${getIntroText(graderName, studentName)}${separator}If anything here that I’ve mentioned is unclear, please don’t hesitate to [reach out for help via Slack.](http://bit.ly/bloc-grading-unstuck) \n\nWant to learn more? Check out our [group sessions & QA resources page](http://bit.ly/gs-g-home) with hours of recorded video and live sessions.\n ${getGraderSignature(graderName)}`;
 
       // get textzarea
       const submissionTextarea = document.getElementById('comment-box');
 
       if (submissionTextarea) {
         // set value of text area with student name
-        submissionTextarea.value = blocDevGraderMessage;
+        submissionTextarea.value = blocDevGraderMessage(studentName);
       }
-    }
-
-    if (isThinkful) {
+    } else if (isThinkfulWebsite) {
       // get textzarea
       const submissionTextarea = document.getElementById('content');
 
@@ -60,17 +62,21 @@ window.onload = function () {
         // Take only the first name
         const studentName = capitalizeFirstLetter(fullStudentName.split(' ')[0]);
 
-        // Dev and Designer messages, each its different
+        // Dev and Designer messages for thinkful, each its different
         const thinkfulDevMessage = `${getIntroText(graderName, studentName)}${contentInTextArea}${separator}If anything here that I’ve mentioned is unclear, please don’t hesitate to join an Q&A session for technical assistance via [Slack](http://bit.ly/tf-dev-grading-help). If it’s a question about the feedback, feel free to resubmit with a question and the grading team will get back to you as quickly as possible. \n\nWant to learn more? Check out our [group sessions & QA resources page](http://bit.ly/gs-g-home) with hours of recorded video and live sessions.\n ${getGraderSignature(graderName)}`;
 
         const thinkfulDesignMessage = `${getIntroText(graderName, studentName)}${contentInTextArea}${separator}If anything here that I’ve mentioned is unclear, please don’t hesitate to reach out for technical assistance via Slack in the [#product-design channel](http://bit.ly/td-pdf-grading-help). If it’s a question about the feedback, feel free to resubmit with a question.\n\nWant to learn more? Check out our [group sessions & QA resources page](http://bit.ly/gs-g-home) with hours of recorded video and live sessions.\n ${getGraderSignature(graderName)}`;
 
-        if (devGrader) {
-          submissionTextarea.value = thinkfulDevMessage;
-        } else if (designGrader) {
-          submissionTextarea.value = thinkfulDesignMessage;
-        } else {
-          submissionTextarea.value = 'Please select your grader type in the extension options';
+        if(isThinkfulGrading) {
+          if (devGrader) {
+            submissionTextarea.value = thinkfulDevMessage;
+          } else if (designGrader) {
+            submissionTextarea.value = thinkfulDesignMessage;
+          } else {
+            submissionTextarea.value = 'Please select your grader type in the extension options';
+          }
+        } else if(isBlocGrading){
+          submissionTextarea.value = blocDevGraderMessage(studentName);
         }
       }
     }
